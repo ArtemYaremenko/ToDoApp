bc439303c9b507c1ca0a9bc30362f6ee6f444415
Artem <artem.yaremenko.1992@gmail.com>
2025-09-09 21:55:55 +0300

    Created controller

---
+++ src/main/java/com/artemyaremenko/todoapp/controllers/ToDoController.java
@@ -0,0 +1,13 @@
+package com.artemyaremenko.todoapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ToDoController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
